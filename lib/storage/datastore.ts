a8aa7bc12474d45d8f45a6bc61fc1a2605628878
Nikolas Rummel <nikolasrummel6@gmail.com>
2023-09-11 21:52:21 +0200

    added deleting of old rooms and guests

--- lib/storage/datastore.ts
+++ lib/storage/datastore.ts
@@ -2,6 +2,12 @@
 import {prisma} from "@/lib/prisma";
 
 export async function createRoom(roomName: string, guestName: string) {
+    // delete inactive rooms
+    // maybe add a scheduler for this logic
+    await deleteInactiveRooms()
+
+
+    // create a new room
     const room = await prisma.room.create({
         data: {
             name: roomName,
@@ -9,9 +15,11 @@ export async function createRoom(roomName: string, guestName: string) {
         },
     });
 
+    // create a new guest and add it to the room
     const guest = await addGuestToRoom(room.id, guestName);
     console.log("Room created: ", JSON.stringify(room));
 
+    // return room and guest
     return {
         "room": room,
         "guest": guest
@@ -27,6 +35,42 @@ export async function addGuestToRoom(roomId: number, name: string) {
         },
     });
 }
+
+async function deleteInactiveRooms() {
+    const threshold = new Date(Date.now() - 24 * 60 * 60 * 1000); // 24h
+
+    try {
+        // Find and delete inactive rooms
+        const inactiveRooms = await prisma.room.findMany({
+            where: {
+                updatedAt: {lte: threshold},
+            },
+        });
+
+        for (const room of inactiveRooms) {
+            // Delete associated guests first
+            await prisma.guest.deleteMany({
+                where: {
+                    roomId: room.id,
+                },
+            });
+
+            // Then, delete the room
+            await prisma.room.delete({
+                where: {
+                    id: room.id,
+                },
+            });
+
+            console.log(`Deleted room ${room.name} and associated guests.`);
+        }
+    } catch (error) {
+        console.error('Error deleting inactive rooms:', error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
 
 /*function createRoomId() {
     return Math.floor(Math.random() * 90000) + 10000;
